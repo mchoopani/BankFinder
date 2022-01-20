774804bd2b1d32e5fd21b2886681ee7f4e86bd4c
Mahmood Choopani <choopani.m81@gmail.com>
2022-01-20 09:12:51 +0330

    add base of things classes

---
+++ Things.h
@@ -0,0 +1,18 @@
+//
+// Created by macho on 1/20/22.
+//
+
+#ifndef FINALPROJECT_THINGS_H
+#define FINALPROJECT_THINGS_H
+
+#endif //FINALPROJECT_THINGS_H
+
+class Point{
+    
+};
+class Bank{
+
+};
+class BankBranch{
+
+};
