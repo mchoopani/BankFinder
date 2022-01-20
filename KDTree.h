093f2df48713a2c037480a724920cd2e9812c391
Mahmood Choopani <choopani.m81@gmail.com>
2022-01-20 09:21:45 +0330

    now Things.h classes has essential fields. KDTree class is also added.

---
+++ KDTree.h
@@ -0,0 +1,12 @@
+//
+// Created by macho on 1/20/22.
+//
+
+#ifndef FINALPROJECT_KDTREE_H
+#define FINALPROJECT_KDTREE_H
+
+#endif //FINALPROJECT_KDTREE_H
+
+class KDTree{
+
+};
--- Things.h
+++ Things.h
@@ -1,18 +1,28 @@
 //
 // Created by macho on 1/20/22.
 //
-
 #ifndef FINALPROJECT_THINGS_H
 #define FINALPROJECT_THINGS_H
-
 #endif //FINALPROJECT_THINGS_H
 
+#include <string>
+#include "KDTree.h"
+using namespace std;
+
 class Point{
-    
+public:
+    int x;
+    int y;
+
 };
 class Bank{
-
+public:
+    Point point;
+    string name;
+    KDTree branches;
 };
 class BankBranch{
-
+    Point point;
+    string bankName;
+    string name;
 };
