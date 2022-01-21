8b4392f04f8866409d1550e685688b59e4f7a372
Mahmood Choopani <choopani.m81@gmail.com>
2022-01-21 10:41:17 +0330

    addBr command added.

--- main.cpp
+++ main.cpp
@@ -49,7 +49,35 @@ int main() {
                 cout << "There is a branch of a bank in this point. please build your bank in other place." << endl;
             }
         }
-
+        else if(command == "addBr"){
+            string bankName, branchName;
+            int x,y;
+            cin >> bankName;
+            cin >> branchName;
+            cin >> x >> y;
+            Bank* bank = nullptr;
+            for (int i = 0; i < ctr; ++i) {
+                if (banks[i]->name == bankName){
+                    bank = banks[i];
+                    break;
+                }
+            }
+            if (bank == nullptr){
+                cout << "Bank Doesn't exist." << endl;
+                continue;
+            }
+            BankBranch* branch = new BankBranch(x,y);
+            branch->name = "Main Branch";
+            branch->bankName = branchName;
+            bool result = allBranches.add(branch);
+            if (result){
+                bank->branches.add(branch);
+                banks[ctr++] = bank;
+                cout << "Branch " << branchName <<" Of bank " << bankName << " successfully added." << endl;
+            } else{
+                cout << "There is a branch of a bank in this point. please build your branch in other place." << endl;
+            }
+        }
 
 
 
