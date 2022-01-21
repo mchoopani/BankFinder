b36b3dec3743624a9b3164991da97545b6f83bb7
Mahmood Choopani <choopani.m81@gmail.com>
2022-01-21 11:58:24 +0330

    toString() method added to BankBranch.

--- Things.h
+++ Things.h
@@ -6,27 +6,41 @@
 
 #include <string>
 #include "KDTree.h"
+
 using namespace std;
 
-class Point{
+class Point {
 public:
     int x;
     int y;
-    bool equals(Point point){
+
+    bool equals(Point point) {
         return point.x == x && point.y == y;
     }
-    Point(int x,int y){
+
+    Point(int x, int y) {
         this->x = x;
         this->y = y;
     }
 };
 
-class BankBranch{
+class BankBranch {
 public:
     Point point;
     string bankName;
     string name;
-    BankBranch(int x, int y) : point(x,y){
+
+    BankBranch(int x, int y) : point(x, y) {
+    }
+
+    void toString() {
+        cout << "+-+-+-+--+-+" << endl;
+        cout << "Bank Name : " << bankName << endl;
+        cout << "Branch Name : " << name << endl;
+        cout << "X: " << point.x << endl;
+        cout << "Y: " << point.y << endl;
+        cout << "+-+-+-+--+-+" << endl;
     }
 };
+
 #endif //FINALPROJECT_THINGS_H
