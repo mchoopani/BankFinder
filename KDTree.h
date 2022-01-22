bb71c9560128838db81346c57521a21cf1edb373
Mahmood Choopani <choopani.m81@gmail.com>
2022-01-22 11:07:43 +0330

    debug delete method.

--- KDTree.h
+++ KDTree.h
@@ -69,6 +69,8 @@ public:
     }
 
     Node *findMinimum(Node *node, bool minBaseX, bool isXBase) {
+        if(node == nullptr)
+            return nullptr;
         if (minBaseX == isXBase) {
             if (node->left != nullptr) {
                 return findMinimum(node->left, minBaseX, !isXBase);
@@ -76,21 +78,21 @@ public:
                 return node;
             }
         } else {
-            Node *minInLeft = findMinimum(node->left, minXBase, !isXBase);
-            Node *minInRight = findMinimum(node->right, minXBase, !isXBase);
+            Node *minInLeft = findMinimum(node->left, minBaseX, !isXBase);
+            Node *minInRight = findMinimum(node->right, minBaseX, !isXBase);
             if (isXBase) {
                 Node *res = node;
-                if (minInLeft != nullptr && minInLeft->point.x < res->point.x)
-                    res = y;
-                if (minInRight != nullptr && minInRight->point.x < res->point.x)
-                    res = z;
+                if (minInLeft != nullptr && minInLeft->branch->point.x < res->branch->point.x)
+                    res = minInLeft;
+                if (minInRight != nullptr && minInRight->branch->point.x < res->branch->point.x)
+                    res = minInRight;
                 return res;
             } else {
                 Node *res = node;
-                if (minInLeft != nullptr && minInLeft->point.y < res->point.y)
-                    res = y;
-                if (minInRight != nullptr && minInRight->point.y < res->point.y)
-                    res = z;
+                if (minInLeft != nullptr && minInLeft->branch->point.y < res->branch->point.y)
+                    res = minInLeft;
+                if (minInRight != nullptr && minInRight->branch->point.y < res->branch->point.y)
+                    res = minInRight;
                 return res;
             }
         }
@@ -99,31 +101,35 @@ public:
     void copySecondInFirstNode(Node *first, Node *second) {
         first->branch = second->branch;
     }
-
+    bool notSeen = true;
     Node *del(Node *node, int x, int y, bool isXBase) {
-        if (node->point.x == x, node->point.y == y) {
+        if (node->branch->point.x == x, node->branch->point.y == y) {
+            if(notSeen && node->branch->name == "Main Branch"){
+                return node;
+            }
+            notSeen = false;
             if (node->right != nullptr) {
                 Node *min = findMinimum(node->right, isXBase, !isXBase);
                 copySecondInFirstNode(node, min);
-                node->right = del(node->right, min->point.x, min->point.y, !isXBase);
+                node->right = del(node->right, min->branch->point.x, min->branch->point.y, !isXBase);
             } else if (node->left != nullptr) {
                 Node *min = findMinimum(node->left, isXBase, !isXBase);
                 copySecondInFirstNode(node, min);
-                node->right = del(node->right, min->point.x, min->point.y, !isXBase);
+                node->right = del(node->right, min->branch->point.x, min->branch->point.y, !isXBase);
             } else {
                 delete node;
                 return nullptr;
             }
         } else {
             if(isXBase){
-                if(x < node->point.x){
+                if(x <= node->branch->point.x){
                     node->left = del(node->left,x,y,!isXBase);
                 }else{
                     node->right = del(node->right,x,y,!isXBase);
                 }
             }
             else{
-                if(y < node->point.y){
+                if(y <= node->branch->point.y){
                     node->left = del(node->left,x,y,!isXBase);
                 }else{
                     node->right = del(node->right,x,y,!isXBase);
@@ -133,7 +139,12 @@ public:
         return node;
     }
     void del(int x,int y){
+        notSeen = true;
         del(root,x,y,true);
+        if(!notSeen){
+            cout << "Main Branch Of Bank Can't be delete." << endl;
+        }
+        notSeen = true;
     }
 
 
