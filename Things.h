a4a72eb7bc702122f66a38f2c8498508eed050d6
Mahmood Choopani <choopani.m81@gmail.com>
2022-01-23 20:12:04 +0330

    improve Area constructor to avoid Exception in user bad input.

--- Things.h
+++ Things.h
@@ -47,10 +47,20 @@ public:
     int x1, x2;
     int y1, y2;
     Area(int x1,int x2, int y1, int y2){
-        this-> x1 = x1;
-        this-> x2 = x2;
-        this-> y1 = y1;
-        this-> y2 = y2;
+        if (x1 < x2) {
+            this->x1 = x1;
+            this->x2 = x2;
+        } else{
+            this->x1 = x2;
+            this->x2 = x1;
+        }
+        if (x1 < x2) {
+            this->x1 = y1;
+            this->x2 = y2;
+        } else{
+            this->x1 = y2;
+            this->x2 = y1;
+        }
     }
     int containsPoint(Point point, bool xBase) {
         if (xBase) {
