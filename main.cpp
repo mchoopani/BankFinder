8b8002ed47bc96d981d32eedce5e0e53cb1bda0f
Mahmood Choopani <choopani.m81@gmail.com>
2022-01-23 11:23:04 +0330

    add 'nearest' command.

--- main.cpp
+++ main.cpp
@@ -1,34 +1,36 @@
 #include <iostream>
 #include "KDTree.h"
 
-class Bank{
+class Bank {
 public:
     Point point;
     string name;
     KDTree branches;
-    Bank(string name,int x,int y): point(x,y){
+
+    Bank(string name, int x, int y) : point(x, y) {
         this->name = name;
     }
 };
+
 int main() {
     KDTree allBranches;
     // TODO: Change Array TO Hash Table
     // ----------------------------------
     int ctr = 0;
-    Bank** banks = new Bank*[1500];
-    while (true){
+    Bank **banks = new Bank *[1500];
+    while (true) {
         string command;
         cin >> command;
-        if (command == "addB"){
+        if (command == "addB") {
             string name;
             cin >> name;
-            int x,y;
-            cin >>x >> y;
+            int x, y;
+            cin >> x >> y;
             // check old banks to avoid duplicate instancing.
             bool continueOuter = false;
             // TODO: change array to hash table to decrease time complexity.
             for (int i = 0; i < ctr; ++i) {
-                if (banks[i]->name == name){
+                if (banks[i]->name == name) {
                     cout << "Bank exists." << endl;
                     continueOuter = true;
                     break;
@@ -36,71 +38,72 @@ int main() {
             }
             if (continueOuter)
                 continue;
-            Bank* bank = new Bank(name,x,y);
-            BankBranch* mainBranch = new BankBranch(x,y);
+            Bank *bank = new Bank(name, x, y);
+            BankBranch *mainBranch = new BankBranch(x, y);
             mainBranch->name = "Main Branch";
             mainBranch->bankName = name;
             bool result = allBranches.add(mainBranch);
-            if (result){
+            if (result) {
                 bank->branches.add(mainBranch);
                 banks[ctr++] = bank;
                 cout << "Main Branch Of bank " << name << " successfully added." << endl;
-            } else{
+            } else {
                 cout << "There is a branch of a bank in this point. please build your bank in other place." << endl;
             }
-        }
-        else if(command == "addBr"){
+        } else if (command == "addBr") {
             string bankName, branchName;
-            int x,y;
+            int x, y;
             cin >> bankName;
             cin >> branchName;
             cin >> x >> y;
-            Bank* bank = nullptr;
+            Bank *bank = nullptr;
             for (int i = 0; i < ctr; ++i) {
-                if (banks[i]->name == bankName){
+                if (banks[i]->name == bankName) {
                     bank = banks[i];
                     break;
                 }
             }
-            if (bank == nullptr){
+            if (bank == nullptr) {
                 cout << "Bank Doesn't exist." << endl;
                 continue;
             }
-            BankBranch* branch = new BankBranch(x,y);
+            BankBranch *branch = new BankBranch(x, y);
             branch->name = branchName;
             branch->bankName = bankName;
             bool result = allBranches.add(branch);
-            if (result){
+            if (result) {
                 bank->branches.add(branch);
                 banks[ctr++] = bank;
-                cout << "Branch " << branchName <<" Of bank " << bankName << " successfully added." << endl;
-            } else{
+                cout << "Branch " << branchName << " Of bank " << bankName << " successfully added." << endl;
+            } else {
                 cout << "There is a branch of a bank in this point. please build your branch in other place." << endl;
             }
-        }
-        else if (command == "listBrs"){
+        } else if (command == "listBrs") {
             string bankName;
             cin >> bankName;
-            Bank* bank = nullptr;
+            Bank *bank = nullptr;
             for (int i = 0; i < ctr; ++i) {
-                if (banks[i]->name == bankName){
+                if (banks[i]->name == bankName) {
                     bank = banks[i];
                     break;
                 }
             }
-            if (bank == nullptr){
+            if (bank == nullptr) {
                 cout << "Bank Not Found." << endl;
                 continue;
             }
             bank->branches.traversal();
-        }
-        else if(command == "delBr"){
-            int x,y;
+        } else if (command == "delBr") {
+            int x, y;
             cin >> x >> y;
-            allBranches.del(x,y);
-        }
-
-        else if(command == "exit"){
+            allBranches.del(x, y);
+        } else if (command == "nearBr") {
+            int x, y;
+            cin >> x >> y;
+            Node *near = allBranches.nearest(x, y);
+            cout << "the nearest branch is " << near->branch->name << " of bank " << near->branch->bankName
+                 << " at dimension " << near->branch->point.x << ", " << near->branch->point.y << endl;
+        } else if (command == "exit") {
             break;
         }
     }
