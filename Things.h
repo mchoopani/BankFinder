80d842752d6438a13961aa56882693b6a32f9496
Mahmood Choopani <choopani.m81@gmail.com>
2022-01-25 21:48:37 +0330

    add new constructor to Area class.

--- Things.h
+++ Things.h
@@ -53,7 +53,23 @@ public:
     string name;
     int x1, x2;
     int y1, y2;
-
+    Area(string name,int x1, int x2, int y1, int y2 ){
+        this->name = name;
+        if (x1 < x2) {
+            this->x1 = x1;
+            this->x2 = x2;
+        } else {
+            this->x1 = x2;
+            this->x2 = x1;
+        }
+        if (y1 < y2) {
+            this->y1 = y1;
+            this->y2 = y2;
+        } else {
+            this->y1 = y2;
+            this->y2 = y1;
+        }
+    }
     Area(int x1, int x2, int y1, int y2) {
         if (x1 < x2) {
             this->x1 = x1;
