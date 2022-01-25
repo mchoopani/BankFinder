d6dab7427ea9fd75ef2280873bdd50e0b05265c9
Mahmood Choopani <choopani.m81@gmail.com>
2022-01-25 23:01:24 +0330

    'get' method of Hashtable added.

--- HashTable.h
+++ HashTable.h
@@ -8,21 +8,35 @@
 #endif //FINALPROJECT_HASHTABLE_H
 
 template<class T>
-class HashTable{
-    T** arr = new T[56];
+class HashTable {
+    T **arr = new T[56];
 public:
-    bool add(T t){
+    bool add(T t) {
         int i = hash(t->name[0]);
-        if(i == -1)
+        if (i == -1)
             return false;
         return true;
     }
-    int hash(char c){
+    T get(string name){
         for (int i = 0; i < 56; ++i) {
-            int index = c % ('z'-'A'+1) + i;
+            int index = (name[0] % hash() + i)%hash();
+            if(arr[index] == nullptr)
+                break;
+            if(arr[index]->name == name)
+                return arr[index];
+        }
+        return nullptr;
+    }
+
+    int hash(char c) {
+        for (int i = 0; i < 56; ++i) {
+            int index = (c % hash() + i) % hash();
             if (arr[i] == nullptr)
                 return index;
         }
         return -1;
+    }
+    int hash(){
+        return ('z' - 'A' + 1);
     }
 };
