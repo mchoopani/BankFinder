4331e772109a62b18b35f0954159503b7a03b0f9
Mahmood Choopani <choopani.m81@gmail.com>
2022-01-29 09:53:28 +0330

    add trie tree to replace hashtable with trie tree.

---
+++ TrieTree.h
@@ -0,0 +1,74 @@
+//
+// Created by macho on 1/29/22.
+//
+
+#ifndef FINALPROJECT_TRIETREE_H
+#define FINALPROJECT_TRIETREE_H
+
+#endif //FINALPROJECT_TRIETREE_H
+template <class T>
+
+class TrieTree{
+    class TrieNode{
+    public:
+        TrieNode** nodes = new TrieNode*[60];
+        int childrenCount = 0;
+        bool isEndOfWord = false;
+        T* data = nullptr;
+
+    };
+    TrieNode* root = nullptr;
+public:
+    void insert(T* newData){
+        if (root == nullptr)
+            root = new TrieNode;
+        TrieNode* cursor = root;
+        for (int i = 0; i < newData->name.size(); ++i) {
+            int index = newData->name[i] - 'A';
+            if(cursor->nodes[index] == nullptr){
+                cursor->nodes[index] = new TrieNode;
+                cursor->childrenCount++;
+            }
+            cursor = cursor->nodes[index];
+        }
+        cursor->isEndOfWord = true;
+        cursor->data = newData;
+    }
+    T* get(string name){
+        TrieNode* cursor = root;
+        for (int i = 0; i < name.size(); ++i) {
+            int index = name[i] - 'A';
+            if(cursor->nodes[index] == nullptr){
+                return nullptr;
+            }
+            cursor = cursor->nodes[index];
+        }
+        return cursor->data;
+    }
+    TrieNode* remove(TrieNode* node, string name, int depth){
+        if(node == nullptr)
+            return nullptr;
+        if(depth == name.size()){
+            node->isEndOfWord = false;
+            if(node->childrenCount==0){
+                delete node;
+                return nullptr;
+            }
+            node->data = nullptr;
+            return node;
+        }
+        bool hasData = node->nodes[name[depth]-'A'] != nullptr;
+        node->nodes[name[depth]-'A'] = remove(node->nodes[name[depth]-'A'],name,depth+1);
+        if (hasData && node->nodes[name[depth]-'A'] == nullptr){
+            node->childrenCount--;
+        }
+        if (node->childrenCount==0 && !node->isEndOfWord){
+            delete node;
+            return nullptr;
+        }
+        return node;
+    }
+    void remove(string name){
+        root = remove(root,name,0);
+    }
+};
